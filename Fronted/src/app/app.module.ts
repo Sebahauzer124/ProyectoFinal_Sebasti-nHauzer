699bd588e51fe247355d85f2959712ec73636b8c
unknown <sebahauzer124@gmail.com>
2022-06-07 21:03:03 -0300

    Fronted

---
+++ Fronted/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { AcercadeComponent } from './components/acercade/acercade.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { SkillComponent } from './components/skill/skill.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { ElabComponent } from './components/elab/elab.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
+ 
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    AcercadeComponent,
+    BannerComponent,
+    SkillComponent,
+    ElabComponent,
+    EducacionComponent,
+    ProyectoComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
