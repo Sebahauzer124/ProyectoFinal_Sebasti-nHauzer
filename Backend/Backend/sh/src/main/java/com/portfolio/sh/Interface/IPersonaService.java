4404a4a393d1b1d083d9c321d998d1707ec7a0d8
unknown <sebahauzer124@gmail.com>
2022-06-11 12:45:20 -0300

    Controlador Persona

---
+++ Backend/Backend/sh/src/main/java/com/portfolio/sh/Interface/IPersonaService.java
@@ -0,0 +1,17 @@
+
+package com.portfolio.sh.Interface;
+
+import com.portfolio.sh.model.Persona;
+import java.util.List;
+
+
+public interface IPersonaService {
+    public List<Persona> getPersonas();
+    
+    public void savePersona(Persona persona);
+    
+    public void deletePersona(Long id);
+    
+    public Persona findPersona(Long id);
+    
+}
---
+++ Backend/Backend/sh/src/main/java/com/portfolio/sh/model/Persona.java
@@ -0,0 +1,25 @@
+ 
+package com.portfolio.sh.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+@Getter @Setter
+@Entity
+public class Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+ private Long Id;
+    
+ 
+ private String nombre;
+ 
+ 
+ private String apellido;
+ 
+ 
+ private String img;
+}
